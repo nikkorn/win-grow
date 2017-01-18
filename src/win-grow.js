e82ab00dc4e48621090eaa4823f415128eda2df4
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2017-01-18 08:50:35 +0000

    Added close and isWindowFilled functions. The toggle function will now, when called with an element other than the one filling the window, will swap the elements.

--- src/win-grow.js
+++ src/win-grow.js
@@ -1,49 +1,95 @@
 (function() {
-  "use strict";
+    "use strict";
 
-  var fn = function (element) {
+    var expandElement = function(element) 
+	{
+        /** Store the element and its original dimensions. */
+        var elementDetails = {
+            element: element,
+            top: element.style.top,
+            bottom: element.style.bottom,
+            left: element.style.left,
+            right: element.style.right,
+            position: element.style.position
+        };
 
-    var elementDetails = {
-      element: element,
-      top: element.style.top,
-      bottom: element.style.bottom,
-      left: element.style.left,
-      right: element.style.right,
-      position: element.style.position
-    };
-
-    Object.assign(element.style, { position: "absolute", top: "0", bottom: "0", left: "0", right: "0" });
-
-    return elementDetails;
-  };
-
-  var wingrow = {
-    fullscreenElement: null,
-    toggle: function (element) {
-
-      if (!this.fullscreenElement)
-      {
-        this.fullscreenElement = fn(element);
-      }
-      else
-      {
-        if (this.fullscreenElement.element !== element)
-        {
-          return;
-        }
-
+        /** Make the element fill the window. */
         Object.assign(element.style, {
-           position: this.fullscreenElement.position,
-           top: this.fullscreenElement.top,
-           bottom: this.fullscreenElement.bottom,
-           left: this.fullscreenElement.left,
-           right: this.fullscreenElement.right
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0"
         });
 
-        this.fullscreenElement = null;
-      }
-    }
-  };
+        return elementDetails;
+    };
 
-  window.wingrow = wingrow;
+    var wingrow = {
+        /** The element which is currently filling the window. */
+        fullscreenElement: null,
+
+        /** Toggle an element to fill the browser window. */
+        toggle: function(element) 
+		{
+			/** Check to see if we already have an element filling our browser window. */
+            if (!this.fullscreenElement) 
+			{
+                this.fullscreenElement = expandElement(element);
+            } 
+			else 
+			{
+				/** Check to see if the supplied element is the one which is filling the window. */
+                if (this.fullscreenElement.element !== element) 
+				{
+					/** We are toggling a different element to the one which is filling the window. */
+					this.close();
+					
+					/** Make the new element fill the window. */
+					this.fullscreenElement = expandElement(element);
+                }
+				else
+				{
+					/** Assign the element its originla dimensions. */
+					Object.assign(element.style, {
+						position: this.fullscreenElement.position,
+						top: this.fullscreenElement.top,
+						bottom: this.fullscreenElement.bottom,
+						left: this.fullscreenElement.left,
+						right: this.fullscreenElement.right
+					});
+
+					/** Remove our full window element. */
+					this.fullscreenElement = null;
+				}
+            }
+        },
+		
+		/** If an element is currently filling the window then close it. */
+        close: function() 
+		{
+			/** Check to see if we even have an element filling our browser window. */
+			if (this.fullscreenElement) 
+			{
+				/** Assign the element its original dimensions. */
+                Object.assign(this.fullscreenElement.element.style, {
+                    position: this.fullscreenElement.position,
+                    top: this.fullscreenElement.top,
+                    bottom: this.fullscreenElement.bottom,
+                    left: this.fullscreenElement.left,
+                    right: this.fullscreenElement.right
+                });
+
+                this.fullscreenElement = null;
+            }
+        },
+		
+		/** Returns whether wingrow is filling the browser window with an element. */
+        isWindowFilled: function() 
+		{
+			return !!this.fullscreenElement;
+        }
+    };
+
+    window.wingrow = wingrow;
 })();
